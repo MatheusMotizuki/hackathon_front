114b91cc8ccaa242661f6cc074e21125ab06be1f
Svaan1 <plricardo504@gmail.com>
2023-10-08 02:56:42 -0300

    Contact on map

--- src/lib/components/MapMarker.svelte
+++ src/lib/components/MapMarker.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Contact from "$lib/components/Contact.svelte";
+
     import("$lib/../app.css")
     import MapIcon from "$lib/assets/book-half.svg"
     import {Marker, Popup} from 'svelte-maplibre';
@@ -14,6 +16,6 @@
         class="border-gray-200 border shadow-2xl focus:outline-2 focus:outline-black w-8 h-8 bg-red-300 text-black rounded-full grid place-items-center"
 >
     <Popup openOn="hover" offset={[0, -10]}>
-        <div class="text-lg font-bold">{school.name}</div>
+        <Contact {school}/>
     </Popup>
 </Marker>
